fe70a0c2772ca80c2dc66a404076342772044340
Sandro Haiden <sandrohc2010@hotmail.com>
2019-09-14 18:13:08 -0400

    Iniciando com Componentes
    Adição de componente Navbar.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar app>
+    <v-toolbar-title>SEMINFO 2019</v-toolbar-title>
+
+    <div class="flex-grow-1"></div>
+
+    <v-toolbar-items>
+      <v-btn text>PROFESSORES</v-btn>
+      <v-btn text>DISCIPLINAS</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "navbar"
+};
+</script>
